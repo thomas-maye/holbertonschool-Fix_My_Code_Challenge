6b646a0d93598a4dde27698edc05303dce90718f
Thomas Maye <thomas.maye.33@gmail.com>
2025-01-13 13:02:45 +0100

    Fix insertion logic in sorting algorithm

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -17,13 +17,11 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
+        if i_arg < result[i]
+            result.insert(i, i_arg)
             is_inserted = true
-            break
         end
+            i += 1
     end
     result << i_arg if !is_inserted
 end
